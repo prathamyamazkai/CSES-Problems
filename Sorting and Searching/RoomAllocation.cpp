31bb77d9a9026822c0a90f3276ab76e42c83bb79
pratham <prathamyamazaki@gmail.com>
2023-02-04 00:29:22 +0530

    Room Allocation Solved

--- Sorting and Searching/RoomAllocation.cpp
+++ Sorting and Searching/RoomAllocation.cpp
@@ -1,4 +1,5 @@
-//Working 
+//interval overlap tracker code
+
 #include <bits/stdc++.h>
 using namespace std ;
 
@@ -7,28 +8,34 @@ int main(){
     cin.tie(0);
 
     int n , a , b;
-    vector<tuple<int,int,int>> v;
+    vector<tuple<int,int,int>> v; //vector of tuples contains 3 integers
+    //start,flag & id , "i:id"
+    
     cin >> n;
 
     for(int i=0 ; i<n ; i++){
         cin >> a >> b;
-        v.push_back({a,0,i});
-        v.push_back({b,1,i});
+        v.push_back({a,0,i}); //flag => 0 (starting) interval ({a,0,i})
+        v.push_back({b,1,i}); //flag => 1 (ending) interval ({b,1,i})
     }
     sort(v.begin(),v.end());
-    int c=0;
-    int ans[n];
-    vector<int>op;
-    for (auto [x,y,i] : v){
+    int c=0; //int c : is used to keep track of open intervals
+
+    int ans[n]; //ans[n] array : is used to store the id(i) of the intervals that overlap at each time
+    
+    vector<int>op; //vector op : is used as stack to keep track of the end times of interval as they finish
+    
+    for (auto [x,y,i] : v){ // The loop iterates over the elements of vector v(range based for loop)
         if (y==0) { //entry
-            c++;
-            if (op.empty()) ans[i]=c;
+            c++; //the loop updates the variable c when y=0 as it indiactes starting(opening interval)
+            
+            if (op.empty()) ans[i]=c; //when the interval is open (starts) we increase as they over lap
             else{
                 ans[i] = op.back(); op.pop_back();
             }
         }
         else {
-            c--;
+            c--; //We decrease as interval ends(closes)
             op.push_back(ans[i]);
         }
     }
@@ -40,12 +47,3 @@ int main(){
     return 0;
 }
 
-//interval overlap tracker code
-
-/*
-vector of tuples contains 3 integers
-start , end & flag
-flag => 0 (starting) interval
-flag => 1 (ending) interval
-
-*/
