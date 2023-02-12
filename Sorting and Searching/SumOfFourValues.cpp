ff158a893a4bc5b43c45fb021f06facd74ec8ffa
pratham <prathamyamazaki@gmail.com>
2023-02-12 19:30:50 +0530

    Sum Of Four Values Solved

--- Sorting and Searching/SumOfFourValues.cpp
+++ Sorting and Searching/SumOfFourValues.cpp
@@ -1,34 +1,41 @@
-//Code needs Optimization - TLE Error
+//Code needs Optimization - TLE Error Resolved by Hash map
+//Using Hash map to make it O(n)
 #include<bits/stdc++.h>
 using namespace std ;
 int main(){
     int n , x ;
     cin >> n >> x ;
-    vector<pair<int,int>>a(n) ;
-    for(int i = 0 ; i<n ; i++){
-        cin >> a[i].first; //input number is stored
-        a[i].second = i+1; //position of each number stored
+    vector<int>a(n) ;
+    for(int &x : a ){
+        cin >> x ;
     }
+    // Store sums of all pairs
+    // in a hash table
+    unordered_map<int , pair<int, int> > mp;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            mp[a[i] + a[j]] = { i, j };
     if(n<3){
         cout<<"IMPOSSIBLE";
         return 0;
     }
-    sort(a.begin(),a.end());
-    for(int f= 0 ; f < n ; f++){
-        for(int k = f+1 ; k<n ; k++){
-            int current = a[f].first+a[k].first;
-            int temp = x-current;
-            int i = 0 , j = n-1 ;
-            while(i<j){
-                if(i==k || i==f) ++i;
-                if(j==k || j==f) --j;
-                if(i>j) break;
-                if(a[i].first + a[j].first == temp){
-                    cout<<a[i].second<<" "<<a[j].second<<" "<<a[f].second<<" "<<a[k].second;
+    // Traverse through all pairs and search
+    // for X - (current pair sum).
+    for (int i = 0; i < n-1 ; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int sum = a[i] + a[j];
+ 
+            // If X - sum is present in hash table,
+            if (mp.find(x - sum) != mp.end()) {
+ 
+                // Making sure that all elements are
+                // distinct array elements and an element
+                // is not considered more than once.
+                pair<int, int> p = mp[x - sum];
+                if (p.first != i && p.first != j && p.second != i && p.second != j){
+                    cout << i+1 << " " << j+1 << " "<< p.first+1 << " "<< p.second+1;
                     return 0;
                 }
-                if(a[i].first + a[j].first >temp) --j;
-                else ++i;
             }
         }
     }
